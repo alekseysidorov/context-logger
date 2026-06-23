4d43e74faa4a7656f071fe922e4a657e103f328f
Aleksey Sidorov <sauron1987@gmail.com>
2026-06-24 01:05:47 +0300

    refactor: rename tests/records.rs and extract smoke test to its own file
    Splitting `test_smoke` into a separate `tests/smoke.rs` avoids multiple
    logger
    initializations within a single file, which is unsupported. This also
    renames the
    original file to `shadowing.rs` to better reflect its content.

--- tests/shadowing.rs
+++ tests/shadowing.rs
@@ -1,20 +1,11 @@
-use context_logger::{LogContext, LogContextExt, LogScope};
+// Warning: Because the each test initializes the logger, we need to split the
+// tests into separate files to avoid multiple initializations of the logger.
+
+use context_logger::{LogContext, LogContextExt};
 
 use crate::common::{RecordExt, check_logger_once};
 
 pub mod common;
-
-#[test]
-fn test_smoke() {
-    check_logger_once(|entry| {
-        let val = entry.get_record("answer").unwrap();
-        assert_eq!(val, 42);
-        Ok(())
-    });
-
-    let _guard = LogScope::enter(LogContext::new().local_record("answer", 42));
-    log::info!("Smoke on the water, fire in the sky");
-}
 
 #[test]
 fn test_inherited_records_shadowing() {
---
+++ tests/smoke.rs
@@ -0,0 +1,20 @@
+// Warning: Because the each test initializes the logger, we need to split the
+// tests into separate files to avoid multiple initializations of the logger.
+
+use context_logger::{LogContext, LogScope};
+
+use crate::common::{RecordExt, check_logger_once};
+
+pub mod common;
+
+#[test]
+fn test_smoke() {
+    check_logger_once(|entry| {
+        let val = entry.get_record("answer").unwrap();
+        assert_eq!(val, 42);
+        Ok(())
+    });
+
+    let _guard = LogScope::enter(LogContext::new().local_record("answer", 42));
+    log::info!("Smoke on the water, fire in the sky");
+}
